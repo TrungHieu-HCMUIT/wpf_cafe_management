a3737b042222f59729838a84c0229a8a5c947526
TrungHieu-HCMUIT <19521515@gm.uit.edu.vn>
2020-11-11 03:27:28 +0700

    Create login and staff screen

---
+++ cafe_management/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cafe_management.ViewModel
+{
+
+    public class MainViewModel : BaseViewModel
+    {
+        //public bool Isloaded = false;
+        public MainViewModel()
+        {
+            //if (!Isloaded)
+            //{
+            //    Isloaded = true;
+            //    LoginWindow loginWindow = new LoginWindow();
+            //    loginWindow.ShowDialog();
+            //}
+        }
+    }
+}
