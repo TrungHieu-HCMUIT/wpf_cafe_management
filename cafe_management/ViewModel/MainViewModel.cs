51562824c66afb4bcb166df9bcab524faa72d50e
TrungHieu-HCMUIT <19521515@gm.uit.edu.vn>
2020-11-12 14:29:02 +0700

    Handle screen event

--- cafe_management/ViewModel/MainViewModel.cs
+++ cafe_management/ViewModel/MainViewModel.cs
@@ -11,10 +11,34 @@ namespace cafe_management.ViewModel
     {
         public ICommand StaffCommand { get; set; }
         public ICommand MenuCommand { get; set; }
+
+        private StaffWindow staffWindow;
+        private MenuWindow menuWindow;
         public MainViewModel()
         {
-            StaffCommand = new RelayCommand<object>((p) => { return true; }, (p) => { StaffWindow wd = new StaffWindow(); wd.ShowDialog(); });
-            MenuCommand = new RelayCommand<object>((p) => { return true; }, (p) => { MenuWindow wd = new MenuWindow(); wd.ShowDialog(); });
+
+            StaffCommand = new RelayCommand<object>((p) =>
+            {
+                return true;
+            }, (p) =>
+            {
+                if (staffWindow == null)
+                {
+                    staffWindow = new StaffWindow();
+                }
+                menuWindow?.Hide();
+                staffWindow.ShowDialog();
+            });
+
+            MenuCommand = new RelayCommand<object>((p) =>
+            {
+                return true;
+            }, (p) =>
+            {
+                menuWindow = new MenuWindow();
+                staffWindow.Hide();
+                menuWindow.ShowDialog();
+            });
         }
     }
 }
