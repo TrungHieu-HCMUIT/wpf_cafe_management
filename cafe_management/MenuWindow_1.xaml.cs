a2b6d92b122d8c795880623376b1bcf3ceebb274
HuyHoang-0 <62274204+HuyHoang-0@users.noreply.github.com>
2020-11-12 09:01:26 +0700

    Create menu screen

---
+++ cafe_management/MenuWindow_1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace cafe_management
+{
+    /// <summary>
+    /// Interaction logic for MenuWindow_1.xaml
+    /// </summary>
+    public partial class MenuWindow_1 : Window
+    {
+        public MenuWindow_1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_DpiChanged(object sender, DpiChangedEventArgs e)
+        {
+
+        }
+    }
+}
